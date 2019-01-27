a62604389ee71ccd6842260af387dcc5c03afc3c
Ekku Sipilä <ekku.eki@gmail.com>
2019-01-27 23:09:13 +0200

    Heroku configuration

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const router = require('./router/router')
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -13,6 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router);
 
-app.listen(3000, () => {
-  console.log(`Server is up on port 3000`);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
